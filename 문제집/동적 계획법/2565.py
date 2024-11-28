c1ddc4174cf60c974fb3948d1c139ee23e606956
kjyook <106497893+kjyook@users.noreply.github.com>
2024-11-28 14:40:16 +0900

    greedy 포기하고... lis사용

--- 문제집/동적 계획법/2565.py
+++ 문제집/동적 계획법/2565.py
@@ -3,47 +3,24 @@ input = sys.stdin.readline
 
 n = int(input())
 line = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
+line.sort()
 
-# dp[i][j] 는 i번째 줄과 j번째 줄이 1-> 만난다 0 -> 안만난다.로 설정한다.
-dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-count = [0] * (n + 1)
-answer = 0
+#dp는 x번째까지 수열에서 최대가 y인 수열의 길이를 나타내는 리스트이다.
+dp = [[0 for _ in range(501)] for _ in range(101)]
+check = set()
 
-#a와 b는 각각 전기줄의 시작점과 끝점을 담은 리스트의 형태
-#return 0 -> 안만남 return 1 -> 만남
-def check(a, b):
-    if a[0] > b[0] and a[1] > b[1]:
-        return 0
-    if a[0] > b[0] and a[1] < b[1]:
-        return 1
-    if a[0] < b[0] and a[1] < b[1]:
-        return 0
-    if a[0] < b[0] and a[1] > b[1]:
-        return 1
+for idx, l in enumerate(line):
+    if idx == 0:
+        continue
 
-for i in range(1, n + 1):
-    for j in range(1, i):
-        if i == j:
-            continue
+    max_int = 0
 
-        if check(line[i], line[j]):
-            dp[i][j] += 1
-            dp[j][i] += 1
-
-for i in range(1, n + 1):
-    count[i] = sum(dp[i])
-
-while True:
-    if sum(count) == 0:
-        break
-
-    idx = count.index(max(count))
-    dp[idx] = [0] * (n + 1)
-
-    for i in range(1, n + 1):
-        dp[i][idx] = 0
-        count[i] = sum(dp[i])
+    for i in range(1, idx):
+        dp[idx][line[i][1]] = dp[idx - 1][line[i][1]]
     
-    answer += 1
+        if line[i][1] < l[1] and max_int < dp[idx][line[i][1]]:
+            max_int = dp[idx][line[i][1]]
 
-print(answer)
+    dp[idx][l[1]] = max_int + 1
+
+print(n - max(dp[n]))
