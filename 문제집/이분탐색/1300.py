dd4e55b43ba888e40c9b98f3e6940f9c3854b560
kjyook <106497893+kjyook@users.noreply.github.com>
2024-12-16 16:24:45 +0900

    일렬로 되어있지 않아도 가능한 이분탐색!

--- 문제집/이분탐색/1300.py
+++ 문제집/이분탐색/1300.py
@@ -3,3 +3,20 @@ input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
+
+left, right = 1, k
+
+while left <= right:
+    mid = (left + right) // 2
+
+    sum = 0
+    for i in range(1, n + 1):
+        sum += min(mid // i, n)
+    
+    if sum >= k:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(answer)
